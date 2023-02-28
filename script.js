dd03de6d0e863696505f652f45064a88c7be105d
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-02-28 17:48:44 +0300

    edit lesson

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 "use strict";
 
-function DomElement(selector) {
+function DomElement(selector, height, width, bg, fontSize) {
     this.selector = selector;
-    this.height = '200';
-    this.width = '200';
-    this.bg = 'red';
-    this.fontSize = '18';
-
-    const div = document.createElement('div');
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
 
     this.create = () => {
         if (this.selector.indexOf('.') !== -1) {
+            const div = document.createElement('div');
+
             div.setAttribute('class', this.selector.slice(1));
-            this.render();
+            div.textContent = 'Любой текст';
+            div.style.cssText = `height: ${this.height}px; width: ${this.width}px; background-color: ${this.bg}; fontSize: ${this.fontSize}px;`;
+            document.body.append(div);
 
         }
         if (this.selector.indexOf('#') !== -1) {
+            const div = document.createElement('p');
+
             div.setAttribute('id', this.selector.slice(1));
-            this.render();
+            div.textContent = 'Любой текст';
+            div.style.cssText = `height: ${this.height}px; width: ${this.width}px; background-color: ${this.bg}; fontSize: ${this.fontSize}px;`;
+            document.body.append(div);
         }
 
     }
-    this.render = () => {
-        div.textContent = 'Любой текст';
-        div.style.cssText = `height: ${this.height}px; width: ${this.width}px; background-color: ${this.bg}; fontSize: ${this.fontSize}px;`;
-        document.body.append(div);
-    }
 }
 
-const el = new DomElement('#test');
+const el = new DomElement('#test', '200', '200', 'red', '18');
+const el2 = new DomElement('.test', '200', '200', 'red', '18');
 el.create();
+el2.create();
